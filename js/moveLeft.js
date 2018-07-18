2ea1a218704bb4fe35d9fd6a513ca9284449b26d
Agan Durmisevic <adurmisevic@kony-dock-17.qs.intra>
2018-07-18 13:18:20 +0200

    left and right bug fixed

--- js/moveLeft.js
+++ js/moveLeft.js
@@ -1,7 +1,7 @@
 function moveLeft() {
     player.pos.x -= 1;
-    drawWorld();
     if (collide(arena, player)) {
         player.pos.x += 1;
     }
+    drawWorld();
 }
--- js/moveRight.js
+++ js/moveRight.js
@@ -1,7 +1,7 @@
 function moveRight() {
     player.pos.x += 1;
-    drawWorld();
     if (collide(arena, player)) {
         player.pos.x -= 1;
     }
+    drawWorld();
 }
