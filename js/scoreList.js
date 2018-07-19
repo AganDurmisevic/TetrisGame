7f3b76196eea11f13f30b13998d9c1bad6f4add9
LeonLammertz <Leon.Lammertz@quinscape.de>
2018-07-19 10:36:29 +0200

    localStorage for highScorelist added

---
+++ js/scoreList.js
@@ -0,0 +1,23 @@
+var scorelist = JSON.parse(localStorage.getItem("scorelist")) || [];
+
+function drawHighscorelist() {
+    scorelist.sort(function(a, b){return a-b});
+    scorelist.reverse();
+    longerThanFive();
+    for (let i = 1; i < scorelist.length + 1; i ++) {
+        document.getElementById('highscorelist').innerHTML += "<br>";
+        document.getElementById('highscorelist').innerHTML += "<h2>" + i + ".&nbsp;&nbsp;" + scorelist[i - 1] + "</h2>";
+        document.getElementById('highscorelist').innerHTML += "<br>";
+    }
+}
+
+function longerThanFive() {
+    while(scorelist.length > 5) {
+        scorelist.pop();
+    }
+}
+
+function addScore() {
+    scorelist.push(score);
+    localStorage.setItem("scorelist", JSON.stringify(scorelist));
+}
