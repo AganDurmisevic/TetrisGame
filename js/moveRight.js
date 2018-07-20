ec9e4f5612ff36f9917a7038be6f84b3d78c0193
Agan Durmisevic <adurmisevic@kony-dock-17.qs.intra>
2018-07-20 15:13:19 +0200

    bug fixed

--- js/moveRight.js
+++ js/moveRight.js
@@ -4,12 +4,8 @@ function moveRight(arena, player) {
     if (collide(arena, player)) {
         player.pos.x -= 1;
     }
-<<<<<<< HEAD
-    drawWorld(player1);
-}
 
-=======
     drawWorld();
     drawWorld2();
 }
->>>>>>> bb27ba59b16f9c8c6b387adcdb9a4a072850869e
+
